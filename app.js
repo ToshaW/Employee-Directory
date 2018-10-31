26b59128ec079c6f0b71d71fe0165185646bd8eb
Washington <221032910@logon.ds.ge.com>
2018-10-31 11:48:33 -0400

    update file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 //1. View option that displays all employee info
 const render = function() {
-    $('.list').empty(); 
+       $('.list').empty(); 
   
     let employee=``;
     for ( let i = 0; i < employeeList.length; i++ ) {
@@ -15,16 +15,29 @@ const render = function() {
   //2. Add option that allows users to input name, office number, and phone number and then renders the updated employee list.
 
   const Add = function() {
-    const name = $('#name').val();
-    employeeList.push(nameVal) {
-      return nodeList[0].value; 
-    } else {
-      nodeList[0].value = content;        
+    const newEmployee = {
+    name: name, 
+    officeNum: officenumber, 
+    phoneNum: phonenumber
+  }
+    const name = $('#Name').val();
+    const officenumber = $('#Office').val();
+    const phonenumber = $('#Phone').val();
+    employeeList.push(newEmployee);
+    render ();   
+ }
+ $('#Submit').on('click', Add());
+ $('#view').on('click', render());
+ 
+  //3. Verify option that allows users to input a name and renders yes if the employee exists and no otherwise.
+  const verify = function() {
+    const nameVal = $('name').val();
+    if(employeeList.includes(nameVal)) {
+    $('name').addClass('yes');
     }
   }
 
-  //3. Verify option that allows users to input a name and renders yes if the employee exists and no otherwise.
-
+  $('search').on('click', render());
 
   //4. Update  option that allows the user to input name, office number, and phone number and updates the office number and phone number of the employee that matches the input name, and then renders the updated employee list.
 
@@ -32,5 +45,9 @@ const render = function() {
   //5. Delete option that deletes the employee with the matching name and then renders the updated employee list.
 
   const removeName = function() {
-      const nameVal = $('name');
+    const nameVal = $('name').val;
+    employeeList.splice(employeeList.indexOf(nameVal),1);
+    $('name').val('')
+    render();
   }
+  $('#delete').on('click', removeName);
